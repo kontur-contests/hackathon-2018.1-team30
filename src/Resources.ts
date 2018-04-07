433dddfc4e76efa6fe0f9672003cdb670990a9bd
Timur Khazamov <t1mmaas@skbkontur.ru>
2018-04-07 18:14:39 +0500

    Added aim

--- src/Resources.ts
+++ src/Resources.ts
@@ -5,6 +5,7 @@ export class Resources {
   public static Audi = new Texture("/assets/sprites/audi.png");
   public static DudeNude = new Texture("/assets/sprites/dude_nude.png");
   public static Level1 = new TiledResource("/assets/levels/1.json");
+  public static Aim = new Texture("/assets/sprites/aim.png");
   // public static GroundTiles = new Texture("/assets/ground_tiles.png");
 
   public static values(): ILoadable[] {
--- src/Scenes/Level1.ts
+++ src/Scenes/Level1.ts
@@ -6,18 +6,24 @@ import {
   TileMap,
   SpriteSheet,
   TileSprite,
-    Vector
+  Vector
 } from "excalibur";
 import Player from "../Chars/Player";
 import { Resources } from "../Resources";
 import SuperCamera from "../SuperCamera";
+import { Aim } from "../Chars/Aim";
 
 export class Level1 extends Scene {
   public onInitialize(engine: Engine) {
     const audi = new Player(1000, 1000);
-      const tileMap: TileMap = Resources.Level1.getTileMap();
-      const tileMapSize = new Vector(tileMap.cellWidth * tileMap.cols, tileMap.cellHeight * tileMap.rows);
-      this.camera.addStrategy(new SuperCamera(audi, tileMapSize, 0.3, 0.9));
+    const tileMap: TileMap = Resources.Level1.getTileMap();
+    const tileMapSize = new Vector(
+      tileMap.cellWidth * tileMap.cols,
+      tileMap.cellHeight * tileMap.rows
+    );
+    this.camera.addStrategy(new SuperCamera(audi, tileMapSize, 0.3, 0.9));
+
+    audi.add(new Aim());
     this.add(new Actor(10, 10, 10, 10, Color.Red));
     this.add(new Actor(-10, 10, 10, 10, Color.Red));
     this.add(new Actor(10, -10, 10, 10, Color.Red));
