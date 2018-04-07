350bbd8ac4edb416946d98577cab2a56be889cd0
Mikhail Shatikhin <shatikhin@skbkontur.ru>
2018-04-08 02:44:53 +0500

    sound

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
-import { Loader } from "excalibur";
+import { Loader, Sound } from "excalibur";
 import { Game } from "./Game";
 import { Resources } from "./Resources";
 import * as signalR from "@aspnet/signalr";
 import { GameService } from "./GameService";
 
+var sound = new Sound("/assets/sound.mp3");
 const game = new Game();
-const loader = new Loader();
+const loader = new Loader([sound]);
+
 loader.addResources(Resources.values());
 
 game.start(loader).then(() => {
     console.log("Started");
     GameService.start();
+});
+
+game.start(loader).then(function () {
+    sound.play();
 });
