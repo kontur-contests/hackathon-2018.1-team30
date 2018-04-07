9af36f5631890862c4c7e4a199fcd21a2a3c4883
Timur Khazamov <t1mmaas@skbkontur.ru>
2018-04-07 15:19:41 +0500

    ADded tile-map to Level1.ts

--- src/Resources.ts
+++ src/Resources.ts
@@ -1,10 +1,13 @@
-import { ILoadable, Texture } from "excalibur";
+import { ILoadable, Texture, Resource } from "excalibur";
+import TiledResource from "@excaliburjs/excalibur-tiled";
 
 export class Resources {
   public static Audi = new Texture("/assets/sprites/audi.png");
   public static DudeNude = new Texture("/assets/sprites/dude_nude.png");
+  public static Level1 = new TiledResource("/assets/levels/1.json");
+  // public static GroundTiles = new Texture("/assets/ground_tiles.png");
 
   public static values(): ILoadable[] {
-    return Object.values(Resources)
+    return Object.values(Resources);
   }
 }
--- src/Scenes/Level1.ts
+++ src/Scenes/Level1.ts
@@ -1,12 +1,10 @@
 import { Engine, Scene } from "excalibur";
 import { Audi } from "../Chars/Audi";
+import { Resources } from "../Resources";
 
 export class Level1 extends Scene {
   public onInitialize(engine: Engine) {
-    const audi = new Audi(
-      100,
-      100
-    );
-    this.add(audi);
+    this.add(Resources.Level1.getTileMap());
+    this.add(new Audi(100, 100));
   }
 }
