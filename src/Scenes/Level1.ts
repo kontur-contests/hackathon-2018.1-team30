8a2f51cfa0a3d8a57c24ece1532766cbd974940f
Mikhail Shatikhin <shatikhin@skbkontur.ru>
2018-04-07 18:02:09 +0500

    joinPlayer

--- src/Scenes/Level1.ts
+++ src/Scenes/Level1.ts
@@ -6,18 +6,20 @@ import {
   TileMap,
   SpriteSheet,
   TileSprite,
-    Vector
+  Vector
 } from "excalibur";
 import Player from "../Chars/Player";
 import { Resources } from "../Resources";
 import SuperCamera from "../SuperCamera";
+import { GameConnections } from "../GameConnections";
+import { IPlayer } from "../models/Player";
 
 export class Level1 extends Scene {
   public onInitialize(engine: Engine) {
     const audi = new Player(1000, 1000);
-      const tileMap: TileMap = Resources.Level1.getTileMap();
-      const tileMapSize = new Vector(tileMap.cellWidth * tileMap.cols, tileMap.cellHeight * tileMap.rows);
-      this.camera.addStrategy(new SuperCamera(audi, tileMapSize, 0.3, 0.9));
+    const tileMap: TileMap = Resources.Level1.getTileMap();
+    const tileMapSize = new Vector(tileMap.cellWidth * tileMap.cols, tileMap.cellHeight * tileMap.rows);
+    this.camera.addStrategy(new SuperCamera(audi, tileMapSize, 0.3, 0.9));
     this.add(new Actor(10, 10, 10, 10, Color.Red));
     this.add(new Actor(-10, 10, 10, 10, Color.Red));
     this.add(new Actor(10, -10, 10, 10, Color.Red));
@@ -25,5 +27,21 @@ export class Level1 extends Scene {
     this.add(audi);
 
     this.add(tileMap);
+
+    GameConnections.connection.on('playerJoin', (player: IPlayer) => {
+      console.log(player);
+      debugger
+      if (player) {
+        const x = player.position.x;
+        const y = player.position.y;
+        const newPlayer = new Player(x, y);
+        this.add(newPlayer);
+      }
+    });
+
   }
+
 }
+
+
+
