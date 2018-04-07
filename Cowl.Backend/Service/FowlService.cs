bad83ed3715f91e87671779f154534442862b1ff
Denis Tarasov <dstarasov@skbkontur.ru>
2018-04-07 23:25:06 +0500

    removed test

--- Cowl.Backend/Service/FowlService.cs
+++ Cowl.Backend/Service/FowlService.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
 
 namespace Cowl.Backend.Service
 {
     public class FowlService
     {
+        public FowlService()
+        {
+            var connection = new HubConnection("");
+        }
+
         public async Task GenerateFowl()
         {
         }
