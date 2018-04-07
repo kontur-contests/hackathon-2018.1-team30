f8e1d2dfcbda0f7403644933a880085e4e15712f
Mikhail Shatikhin <shatikhin@skbkontur.ru>
2018-04-07 18:17:17 +0500

    joinPlayer

--- src/Scenes/Level1.ts
+++ src/Scenes/Level1.ts
@@ -16,9 +16,11 @@ import { IPlayer } from "../models/Player";
 
 export class Level1 extends Scene {
   public onInitialize(engine: Engine) {
-    const audi = new Player(1000, 1000);
     const tileMap: TileMap = Resources.Level1.getTileMap();
     const tileMapSize = new Vector(tileMap.cellWidth * tileMap.cols, tileMap.cellHeight * tileMap.rows);
+    this.add(tileMap);
+
+    const audi = new Player(1000, 1000);
     this.camera.addStrategy(new SuperCamera(audi, tileMapSize, 0.3, 0.9));
     this.add(new Actor(10, 10, 10, 10, Color.Red));
     this.add(new Actor(-10, 10, 10, 10, Color.Red));
@@ -26,16 +28,14 @@ export class Level1 extends Scene {
     this.add(new Actor(-10, -10, 10, 10, Color.Red));
     this.add(audi);
 
-    this.add(tileMap);
-
     GameConnections.connection.on('playerJoin', (player: IPlayer) => {
       console.log(player);
-      debugger
       if (player) {
         const x = player.position.x;
         const y = player.position.y;
         const newPlayer = new Player(x, y);
         this.add(newPlayer);
+        this.camera.addStrategy(new SuperCamera(newPlayer, tileMapSize, 0.3, 0.9));
       }
     });
 
