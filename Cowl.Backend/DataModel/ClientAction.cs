b53a962cf0fc59a46339349e5f023943b93bb8a5
slawwan <slawwan@skobkontur.ru>
2018-04-07 17:46:39 +0500

    клиентские действия

---
+++ Cowl.Backend/DataModel/ClientAction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+
+namespace Cowl.Backend.DataModel
+{
+    public interface IClientAction
+    {
+    }
+    
+    public class PlayerdMove: IClientAction
+    {
+        public Guid PlayerId { get; set; }
+        public Point To { get; set; }
+    }
+    
+    public class FowlMove: IClientAction
+    {
+        public Guid FowlId { get; set; }
+        public Point To { get; set; }
+    }
+    
+    public class PlayerConnected: IClientAction
+    {
+        public Guid PlayerId { get; set; }
+        public string Name { get; set; }
+        public Point Position { get; set; }
+    }
+    
+    public class PlayerDisconnected: IClientAction
+    {
+        public Guid PlayerId { get; set; }
+    }
+}
