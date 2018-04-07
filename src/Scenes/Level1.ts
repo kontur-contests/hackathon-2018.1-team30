ce0ea05b3efdfb7e23f42d5162638eee6336093d
Timur Khazamov <t1mmaas@skbkontur.ru>
2018-04-07 16:57:34 +0500

    Downgrader to excalibur 0.14

--- src/Scenes/Level1.ts
+++ src/Scenes/Level1.ts
@@ -1,6 +1,15 @@
-import { Engine, Scene, Actor, Color, TileMap } from "excalibur";
+import {
+  Engine,
+  Scene,
+  Actor,
+  Color,
+  TileMap,
+  SpriteSheet,
+  TileSprite
+} from "excalibur";
 import Player from "../Chars/Player";
 import { Resources } from "../Resources";
+import { spriteSheet } from "../SpriteSheets/DudeNudeSpriteSheet";
 
 export class Level1 extends Scene {
   public onInitialize(engine: Engine) {
@@ -12,5 +21,7 @@ export class Level1 extends Scene {
     this.add(new Actor(10, -10, 10, 10, Color.Red));
     this.add(new Actor(-10, -10, 10, 10, Color.Red));
     this.add(audi);
+
+    this.add(Resources.Level1.getTileMap());
   }
 }
--- src/SpriteSheets/DudeNudeSpriteSheet.ts
+++ src/SpriteSheets/DudeNudeSpriteSheet.ts
@@ -1,42 +1,66 @@
-import {Engine, SpriteSheet} from "excalibur";
-import {Resources} from "../Resources";
+import { Engine, SpriteSheet } from "excalibur";
+import { Resources } from "../Resources";
 
 const width = 64;
 const height = 64;
 const columns = 13;
 const rows = 21;
 
-const spriteSheet = new SpriteSheet({
-    columns,
-    rows,
-    spHeight: height,
-    spWidth: width,
-    image: Resources.DudeNude
-});
+export const spriteSheet = new SpriteSheet(
+  Resources.DudeNude,
+  columns,
+  rows,
+  width,
+  height
+);
 
-function getIndex(row: number, column: number = 0) : number {
-    return columns * row + column;
+function getIndex(row: number, column: number = 0): number {
+  return columns * row + column;
 }
 
 export default {
-    width,
-    height,
-    columns,
-    rows,
-    idle: {
-        down: () => spriteSheet.getSprite(getIndex(2)),
-        down_left: () => spriteSheet.getSprite(getIndex(2)),
-        down_right: () => spriteSheet.getSprite(getIndex(2)),
-        up: () => spriteSheet.getSprite(getIndex(0)),
-        up_left: () => spriteSheet.getSprite(getIndex(0)),
-        up_right: () => spriteSheet.getSprite(getIndex(0)),
-        left: () => spriteSheet.getSprite(getIndex(1)),
-        right: () => spriteSheet.getSprite(getIndex(3))
-    },
-    walk: {
-        left: (engine: Engine, speed: number = 75) => spriteSheet.getAnimationBetween(engine, getIndex(9), getIndex(9, 9), speed),
-        right: (engine: Engine, speed: number = 75) => spriteSheet.getAnimationBetween(engine, getIndex(11), getIndex(11, 9), speed),
-        up: (engine: Engine, speed: number = 75) => spriteSheet.getAnimationBetween(engine, getIndex(8), getIndex(8, 9), speed),
-        down: (engine: Engine, speed: number = 75) => spriteSheet.getAnimationBetween(engine, getIndex(10), getIndex(10, 9), speed)
-    }
+  width,
+  height,
+  columns,
+  rows,
+  idle: {
+    down: () => spriteSheet.getSprite(getIndex(2)),
+    down_left: () => spriteSheet.getSprite(getIndex(2)),
+    down_right: () => spriteSheet.getSprite(getIndex(2)),
+    up: () => spriteSheet.getSprite(getIndex(0)),
+    up_left: () => spriteSheet.getSprite(getIndex(0)),
+    up_right: () => spriteSheet.getSprite(getIndex(0)),
+    left: () => spriteSheet.getSprite(getIndex(1)),
+    right: () => spriteSheet.getSprite(getIndex(3))
+  },
+  walk: {
+    left: (engine: Engine, speed: number = 75) =>
+      spriteSheet.getAnimationBetween(
+        engine,
+        getIndex(9),
+        getIndex(9, 9),
+        speed
+      ),
+    right: (engine: Engine, speed: number = 75) =>
+      spriteSheet.getAnimationBetween(
+        engine,
+        getIndex(11),
+        getIndex(11, 9),
+        speed
+      ),
+    up: (engine: Engine, speed: number = 75) =>
+      spriteSheet.getAnimationBetween(
+        engine,
+        getIndex(8),
+        getIndex(8, 9),
+        speed
+      ),
+    down: (engine: Engine, speed: number = 75) =>
+      spriteSheet.getAnimationBetween(
+        engine,
+        getIndex(10),
+        getIndex(10, 9),
+        speed
+      )
+  }
 };
