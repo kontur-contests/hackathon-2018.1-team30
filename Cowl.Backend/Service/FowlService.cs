9390759ec8812193e16c15f60ed1b08653f23b0f
Denis Tarasov <dstarasov@skbkontur.ru>
2018-04-07 23:26:28 +0500

    added test

--- Cowl.Backend/Service/FowlService.cs
+++ Cowl.Backend/Service/FowlService.cs
@@ -10,7 +10,7 @@ namespace Cowl.Backend.Service
     {
         public FowlService()
         {
-            var connection = new HubConnection("");
+            var connection = new HubConnectionBuilder.
         }
 
         public async Task GenerateFowl()
